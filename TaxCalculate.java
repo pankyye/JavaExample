4e1595100b566811cbf1ef3a9a72292a1b17812c
pankyye <ye.nan0910@gmail.com>
2021-12-07 20:42:48 +0800

    Add description

--- TaxCalculate.java
+++ TaxCalculate.java
@@ -1,3 +1,5 @@
+//需求：根据不同收入类型，计算出总纳税金额
+
 class Main {
     public  static  void main(String[] args){
         Income[] incomes = new Income[]{
@@ -8,7 +10,7 @@ class Main {
         System.out.println(totalTax(incomes));
     }
 
-    public static double totalTax(Income... incomes){
+    public static double totalTax(Income... incomes){ //Income后面的三个点是什么意思？
         double total = 0;
         for(Income income: incomes){
             total = total + income.getTax();
@@ -54,3 +56,9 @@ class StateCouncilSpecialAllowance extends Income{
         return 0;
     }
 }
+
+/*
+观察totalTax()方法：利用多态，totalTax()方法只需要和Income打交道，它完全不需要知道Salary和StateCouncilSpecialAllowance的存在，
+就可以正确计算出总的税。如果我们要新增一种稿费收入，只需要从Income派生，然后正确覆写getTax()方法就可以。把新的类型传入totalTax()，
+不需要修改任何代码。
+*/
